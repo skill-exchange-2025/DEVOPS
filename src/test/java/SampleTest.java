deaa0aec68a94da67406756c26d3a232e1e8fdef
oookbaaa <okba.bedhiafi@esprit.tn>
2025-02-22 14:28:09 +0100

    Add JUnit test dependency and sample test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
